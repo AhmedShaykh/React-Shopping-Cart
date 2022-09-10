aa031afbccda8f1edbe5ef7c58b699763573192f
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-09-10 19:49:40 +0500

    Git Init

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <h1>Shopping Cart TypeScript</h1>
+      
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis doloribus esse unde rem ratione, quaerat inventore, laborum aliquid tenetur numquam molestiae perspiciatis totam odio quos adipisci sint animi asperiores commodi.</p>
     </div>
   );
 }
